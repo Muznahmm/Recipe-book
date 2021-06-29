35a27a76bc9e93aa19cef12687508346b12b60e9
muznahmm <muznahm94@gmail.com>
2021-06-29 22:56:54 +0530

    Finish navbar styles and routing

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface NavLink {
+  name: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -11,5 +16,16 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  links: NavLink[] = [
+    {
+      name: 'Contacts',
+      path: '/contacts',
+    },
+    {
+      name: 'Transactions',
+      path: '/transactions',
+    }
+  ];
 
 }
