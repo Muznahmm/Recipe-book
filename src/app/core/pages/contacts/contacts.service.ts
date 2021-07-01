faf0e9747a365739abced941fc6f20e59389d6cb
muznahmm <muznahm94@gmail.com>
2021-07-01 17:43:00 +0530

    Fetch and get contact name using contact id

--- src/app/core/pages/contacts/contacts.service.ts
+++ src/app/core/pages/contacts/contacts.service.ts
@@ -21,41 +21,82 @@ export class ContactsService {
     ) { }
 
     fetchContacts() {
-        return this.http.get(API.GET_CONTACTS) as Observable<ContactsData>;
+        return this.http.get<ContactsData>(API.GET_CONTACTS)
+        .pipe(
+            tap(res => {
+                for(const contact of res.contacts) {
+                    /**
+                     * List all contact that are being added 
+                     * to the private array of contact
+                     */
+                    this.updateContactList(contact);
+                }
+            })
+        );
     }
 
     fetchContactById(id: number) {
         return this.http.get<ContactData>(`${API.GET_CONTACT}/${id}`)
-        // .pipe(
-        //     tap( contact => {
-        //         //this.updateContactList(contact);
-        //     })
-        // )
+        .pipe(
+            tap( contact => {
+                this.updateContactList(contact);
+            })
+        )
     }
 
-    // getContactById(id: number): ContactData | undefined {
-    //     const contact = this.contacts.find(c => c.id === id);
+    getContactById(id: number): ContactData | undefined {
+        const contact = this.contacts.find(c => c.id === id);
 
-    //     if(!contact) {
-    //         if(this.contactIdsBeingFetched.indexOf(id) === -1) {
-    //             this.contactIdsBeingFetched.push(id);
-    //             this.fetchContactById(id).subscribe()
-    //         }
-    //     }
-    // }
+        if(!contact) {
+            /** 
+             * if there is no contact id then then 
+             * it will fetch other wise it return contact
+            */
+            if(this.contactIdsBeingFetched.indexOf(id) === -1) {
+                this.contactIdsBeingFetched.push(id);
+                this.fetchContactById(id).subscribe()
+            }
+        }
 
-    // updateContactList(contact: ContactData) {
-    //     const indexOfContact = this.contacts.find(c => c.id === contact.id);
+        return contact;
+    }
 
-    //     if(indexOfContact === -1) {
-    //         this.contacts.push(contact);
-    //     } else {
-    //         this.contacts.indexOf[indexOfContact] = contact; 
-    //     }
+    private updateContactList(contact: ContactData) {
+        // It will give new contact that stored in backend.
+        const indexOfContact = this.contacts.findIndex(c => c.id === contact.id);
+
+        // Update the contact in list
+        if(indexOfContact === -1) {
+            this.contacts.push(contact);
+        } else {
+            this.contacts[indexOfContact] = contact; 
+        }
+        // it check if there is a contact that is being fetched in contactIdBeingFetched
+        const indexOfIdBeingFetched = this.contactIdsBeingFetched.indexOf(contact.id);
+        // If there is  then it removes from contactIdsBeingFetched array
+        if(indexOfIdBeingFetched !== -1) {
+            this.contactIdsBeingFetched.splice(indexOfIdBeingFetched, 1);
+        }
         
-    // }
+    }
 
     deleteContact(id: number) {
-        return this.http.delete<{success: boolean}>(`${API.DELETE_CONTACT}/${id}`);
+        return this.http.delete<{success: boolean}>(`${API.DELETE_CONTACT}/${id}`)
+        .pipe(
+            tap(res => {
+                if(res.success){
+                    // Remove Contacts from service list
+                    this.removeContactFromList(id)
+                }
+            })
+        );
+    }
+
+    removeContactFromList(id: number) {
+        const index = this.contacts.findIndex(c => c.id === id );
+        // It remove contacts from contacts array
+        if(index !== -1) {
+            this.contacts.splice(index, 1);
+        }
     }
 }
--- src/app/core/pages/transactions/transaction/transaction.component.ts
+++ src/app/core/pages/transactions/transaction/transaction.component.ts
@@ -1,17 +1,63 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TransactionData, TransactionTypeCode } from 'src/app/helpers/types';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/Operators';
+
+
+import { ContactData, TransactionData, TransactionTypeCode } from 'src/app/helpers/types';
+import { ContactsService } from '../../contacts/contacts.service';
+
 
 @Component({
-  selector: 'app-transaction',
+  selector: 'mm-transaction',
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss']
 })
 export class TransactionComponent implements OnInit {
   @Input('transaction') txn!: TransactionData;
   @Input('isLast') isLast!: boolean;
-  constructor() { }
+  //To hide contact name that already have contact name mentioned above
+  @Input('hideContactName') hideName!: boolean; 
 
-  ngOnInit(): void {
+  private fetchContactHttp$!: Observable<ContactData>
+  
+  constructor(
+    private contactService: ContactsService,
+  ) { }
+
+  ngOnInit(){
+    this.fetchContactHttp$ = this.contactService
+    .fetchContactById(this.txn.contactId)
+    .pipe(
+      shareReplay()
+    );
+  }
+
+  getContactName(){
+    //It triggers view life cycle when any changes is happen so it is a bad way
+    
+    // return this.contactService
+    // .fetchContactById(this.txn.contactId)
+    // .subscribe( res => {
+    //   return res.firstName;
+    // })
+
+    //Better approach 
+    //ShareReplay: It give API call response as replay
+    // return this.fetchContactHttp$
+    // .subscribe(res => {
+    //   console.log(res);
+    //   // The O/p show [object object] because it return Observable so better way to show a name is 
+    //   return res.firstName;
+    // })
+
+    // return this.fetchContactHttp$
+    // .pipe(
+    //   map( res => `${res.firstName} ${res.lastName}`),
+    // );
+
+    // Using much better method by state transfer
+    const contact = this.contactService.getContactById(this.txn.contactId);
+    return contact ? `${contact.firstName} ${contact.lastName}` : 'Loading';
   }
 
   get containerStyleClass() {
@@ -33,4 +79,6 @@ export class TransactionComponent implements OnInit {
   get description() {
     return this.txn.description ? this.txn.description : 'No description provided!';
   }
+
+  
 }
